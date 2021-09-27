a5a8e6138d4a2ae49b2328eceab287df13b40d0d
felipecruz91 <felipecruz91@hotmail.es>
2021-09-27 18:13:00 +0200

    display containers in table

--- tailscale/client/src/script.js
+++ tailscale/client/src/script.js
@@ -26,15 +26,74 @@ function logoutTailscale() {
     .then(() => updatetStatus());
 }
 
+var jsonStatus = "";
+
 function updatetStatus() {
   window.ddClient
     .backend("tailscale")
-    .execInContainer("tailscale_service", `/app/tailscale status`)
+    .execInContainer("tailscale_service", `/app/tailscale status -json`)
     .then((value) => {
-      document.getElementById("status").innerText = value.stdout;
+      console.log(value.stdout);
+      jsonStatus = JSON.parse(value.stdout);
     })
     .catch((err) => {
       console.log(err);
       document.getElementById("status").innerText = "Logged out";
     });
+}
+
+var containers = "";
+
+function listContainers() {
+  window.ddClient
+    .listContainers()
+    .then((value) => {
+      containers = value;
+      createTable();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function createTable() {
+  var body = document.body,
+    tbl = document.createElement("table");
+  tbl.style.width = "100%";
+
+  const headers = ["Container", "Published ports", "Magic DNS", "Tailscale IP"];
+
+  var headerRow = tbl.insertRow();
+  for (var i = 0; i < headers.length; i++) {
+    var td = headerRow.insertCell();
+    td.innerText = headers[i];
+  }
+
+  for (var i = 0; i < containers.length; i++) {
+    var tr = tbl.insertRow();
+
+    var td = tr.insertCell();
+    td.innerText = containers[i].Names[0].substring(1);
+
+    td = tr.insertCell();
+    for (var j = 0; j < containers[i].Ports.length; j++) {
+      td.innerText = containers[i].Ports[j].PublicPort + ",";
+    }
+
+    var td = tr.insertCell();
+    for (var j = 0; j < containers[i].Ports.length; j++) {
+      var url =
+        "http://" +
+        jsonStatus.Self.DNSName.slice(0, -1) +
+        ":" +
+        containers[i].Ports[j].PublicPort;
+
+      td.innerHTML = `<a href="${url}" target="_blank">${url}</a>`;
+    }
+
+    var td = tr.insertCell();
+    td.innerText = jsonStatus.Self.TailscaleIPs[0]; //ipv4
+    td.innerText = td.innerText + "\n" + jsonStatus.Self.TailscaleIPs[1]; //ipv6
+  }
+  body.appendChild(tbl);
 }

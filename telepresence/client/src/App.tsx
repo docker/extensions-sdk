204cdc95f761d9913bc77a65d32a100a61e49f72
felipecruz91 <felipecruz91@hotmail.es>
2021-09-30 12:58:33 +0200

    use host binaries and remove docker-compose.yaml

--- telepresence/client/src/App.tsx
+++ telepresence/client/src/App.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
 export function App() {
-  return <h1>Hello</h1>;
+  return (
+    <React.Fragment>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Telepresence</h1>
+        <h2>
+          Debug your Kubernetes service locally, using your favorite debugging
+          tool.
+        </h2>
+      </div>
+
+      <div>
+        Remote Kubernetes clusters:
+        <ul>
+          <li>Cluster 1</li>
+          <li>Cluster 2</li>
+          <li>Cluster 3</li>
+        </ul>
+      </div>
+
+      <div>
+        Containers:
+        <ul>
+          <li>Container 1</li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 }

aa93952e91bcb35d1ca257149ff0faef5893deec
felipecruz91 <felipecruz91@hotmail.es>
2021-09-30 17:52:05 +0200

    implement intercept/leave functionality

--- telepresence/client/src/App.tsx
+++ telepresence/client/src/App.tsx
@@ -1,21 +1,105 @@
 import React, { useState, useEffect } from 'react';
 
+// Kubernetes service that can be intercepted by Telepresence
+export interface Service {
+  Name: string;
+  Port: number;
+  Intercepted: boolean;
+}
+
 export function App() {
   const [cluster, setCluster] = useState<string>('');
+  const [services, setServices] = useState<Service[]>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    if (cluster == '') {
-      connect();
+    if (!loaded) {
+      list();
+      setLoaded(true);
     }
-  }, [cluster]); // Only re-run the effect if Tailscale status changes
+  }, [services]); // Only re-run the effect if intercepted services changes
 
-  function connect() {
-    console.log(`connecting to remote Kubernetes cluster ${cluster}`);
-    console.log(window.ddClient.backend('telepresence'));
+  function list() {
+    console.log(
+      `listing current intercepts (they could be intercepted or not)`,
+    );
+
     window.ddClient
       .backend('telepresence')
-      .execHostCmd('telepresence list')
-      .then((value: any) => console.log(value))
+      .execHostCmd(`telepresence list | grep 'intercept'`)
+      .then((value: any) => {
+        let services: Service[] = [];
+        let strs = value.stdout.split('\n');
+
+        for (var i = 0; i < strs.length; i++) {
+          if (strs[i].length > 0) {
+            let line = strs[i].split(':');
+            let name = line[0].trimEnd();
+            let description = line[1].trimEnd();
+
+            const service: Service = {
+              Name: name,
+              Port: 8080, // TODO: Get port from kubectl?
+              Intercepted: false,
+            };
+
+            if (description.includes('intercepted')) {
+              service.Intercepted = true;
+            }
+
+            services.push(service);
+          }
+        }
+
+        setServices(services);
+      })
+      .catch((err: Error) => {
+        console.log(err);
+      });
+  }
+
+  function intercept(serviceName: string, port: number) {
+    console.log(`intercepting service ${serviceName} on port ${port}`);
+    window.ddClient
+      .backend('telepresence')
+      .execHostCmd(`telepresence intercept ${serviceName} --port ${port}`)
+      .then((value: any) => {
+        console.log(value.stdout);
+
+        let updatedServices = services.map((s) => {
+          if (s.Name == serviceName) {
+            s.Intercepted = true;
+          }
+          return s;
+        });
+
+        console.log(updatedServices);
+
+        setServices(updatedServices);
+      })
+      .catch((err: Error) => {
+        console.log(err);
+      });
+  }
+
+  function leave(serviceName: string) {
+    console.log(`stopping to intercept service ${serviceName}`);
+    window.ddClient
+      .backend('telepresence')
+      .execHostCmd(`telepresence leave ${serviceName}`)
+      .then((value: any) => {
+        console.log(value.stdout);
+        let updatedServices = services.map((s) => {
+          if (s.Name == serviceName) {
+            s.Intercepted = false;
+          }
+          return s;
+        });
+
+        console.log(updatedServices);
+
+        setServices(updatedServices);
+      })
       .catch((err: Error) => {
         console.log(err);
       });
@@ -37,6 +121,31 @@ export function App() {
           <li>Cluster 1</li>
           <li>Cluster 2</li>
           <li>Cluster 3</li>
+        </ul>
+      </div>
+
+      <div>
+        Kubernetes services:
+        <ul>
+          {services.map((s) => (
+            <React.Fragment>
+              <li>Name: {s.Name}</li>
+              <button
+                type="button"
+                onClick={() => intercept(s.Name, s.Port)}
+                disabled={s.Intercepted}
+              >
+                Intercept
+              </button>
+              <button
+                type="button"
+                onClick={() => leave(s.Name)}
+                disabled={!s.Intercepted}
+              >
+                Leave
+              </button>
+            </React.Fragment>
+          ))}
         </ul>
       </div>
 

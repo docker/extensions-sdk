2a2216471d3d1af1532d29c248da89d9ece44f0e
Guillaume Tardif <guillaume.tardif@gmail.com>
2021-10-18 16:22:37 +0200

    Remove hardcoded paths for windows install
    Signed-off-by: Guillaume Tardif <guillaume.tardif@gmail.com>

--- telepresence/telepresence-win-install.ps1
+++ telepresence/telepresence-win-install.ps1
@@ -1,4 +1,9 @@
+# Resolve current folder
+$rootFolder = (Get-Item .).FullName
+echo $rootFolder
+
 # Unzip the zip file to a suitable directory + cleanup zip
+cd windows
 Expand-Archive -Path telepresence.zip
 Remove-Item 'telepresence.zip'
 cd telepresence
@@ -7,7 +12,7 @@ cd telepresence
 # It will install telepresence to C:\telepresence by default, 
 # but you can specify a custom path $path with -Path $path
 Set-ExecutionPolicy Bypass -Scope Process
-.\install-telepresence.ps1 -Path "C:\Users\felipe\AppData\Roaming\Docker\Desktop\plugins\telepresence\host"
+.\install-telepresence.ps1 -Path $rootFolder
 
 # Remove the unzipped directory
 cd ..

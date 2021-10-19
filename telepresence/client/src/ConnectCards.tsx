1291ba462a89ed26a0fe2a35a5b76df258dd03c7
njayp <nickjaypowell@gmail.com>
2021-10-19 09:11:25 -0700

    simple router and material
    Signed-off-by: njayp <nickjaypowell@gmail.com>
    
    rebase

---
+++ telepresence/client/src/ConnectCards.tsx
@@ -0,0 +1,16 @@
+import {CardProps} from "./Connect"
+
+export const cards: CardProps[] = [
+    {
+        "title": "Connect",
+        "body": "Connect to docker-desktop. This will install the traffic manager blah blah",
+        "button" : "Connect",
+        "buttonLink" : "/intercepts"
+    },
+    {
+        "title": "Quickstart",
+        "body": "Install a telepresence demo in your docker cluster, you will be guided on how to use blah blah",
+        "button" : "Get Started",
+        "buttonLink" : "/"
+    }
+]

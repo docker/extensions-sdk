638270468163bc33f70612c02d585d14fef30d57
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2022-03-21 12:26:15 +0100

    Upgrade react-extension sample

--- samples/react-extension/client/src/App.tsx
+++ samples/react-extension/client/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Box, Button } from '@mui/material';
 
 export function App() {
   function sayHello() {
     alert('Hello, World!');
   }
 
-  return <button onClick={sayHello}>Click me!</button>;
+  return (
+    <Box
+      display="flex"
+      flexGrow={1}
+      justifyContent="center"
+      alignItems="center"
+      height="100vh">
+      <Button variant="contained" onClick={sayHello}>Click me!</Button>
+    </Box>
+  );
 }
